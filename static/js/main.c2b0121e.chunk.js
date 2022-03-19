de5f79821414acbe991975386e4b4b5fcf1b5211
Code Sandbox <hello@codesandbox.io>
2022-03-19 21:04:58 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.c2b0121e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdota-chatwheel-by-line"]=this["webpackJsonpdota-chatwheel-by-line"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),s=n(3),a=n.n(s),i=(n(9),n(0));function h(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"Dota 12v12 Chatwheel"})})}var o=n(4),r=n.n(o),d=document.getElementById("root");a.a.render(Object(i.jsxs)(c.StrictMode,{children:[Object(i.jsx)(h,{}),Object(i.jsx)(r.a,{})]}),d)},4:function(e,t){},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.c2b0121e.chunk.js.map
